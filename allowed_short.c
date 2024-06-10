9aec1a6aebd9895487dff6b59dc7fbbbccde3962
Shrushti Kadam <kadamshrush@gmail.com>
2024-06-11 00:05:08 +0530

    Add files via upload

---
+++ allowed_short.c
@@ -0,0 +1,39 @@
+signed char c; 
+char c; 
+#--------------------
+singed short int s; 
+short s; 
+#--------------------
+unsigned short int us; 
+unsigned short us; 
+#---------------------
+signed int n; 
+int n; 
+#-----------------------
+signed long int lng_n;
+long lng_n; 
+#--------------------------
+unsigned long int u_lng_n; 
+unsigned long u_lng_n; 
+#-------------------------
+signed long long int v_lng_n; 
+long long v_lng_int; 
+#-------------------------
+unsigned long long int u_v_lng_n; 
+unsigned long long u_v_lng_n; 
+#----------------------------------
+char    // 1 
+short   // 2 
+int     // 4 
+long    // 4 
+long long   // 8 
+#------------------------------------
+unsigned char   // 1 
+unsigned short  // 2 
+unsigned int    // 4     
+unsigned long   // 4 
+unsigned long long  // 8 
+#------------------------------------
+How to store fractional number? 
+IEEE -> 754 
+#------------------------------------
